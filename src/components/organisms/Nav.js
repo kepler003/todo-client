ec2762e713a5851a405510e906031b9cf0154da8
Bartłomiej Pyzocha <b.pyzocha@gmail.com>
2020-09-25 18:41:43 +0200

    Create log out functionality

--- src/components/organisms/Nav.js
+++ src/components/organisms/Nav.js
@@ -1,22 +1,45 @@
-import React, {useContext} from 'react';
-import Button from '../atoms/Button';
-import AppContext from '../../contexts/AppContext';
+import React, {useContext} from 'react'
+import AppContext from '../../contexts/AppContext'
+import axios from 'axios'
 
-import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import Button from '../atoms/Button'
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 function Nav() {
 
-  const {context} = useContext(AppContext);
+  const {context, setContext} = useContext(AppContext)
+
+  const handleLogOut = async () => {
+    
+    try {
+
+      await axios.post('/user/logout')
+
+    } catch (err) {
+
+      console.log(err.response.data.message || err)
+
+    } finally {
+      
+      setContext({
+        user: {
+          id: null,
+          username: null
+        },
+        notes: null
+      })
+    }
+  }
 
   return (
     <nav className='nav'>
       <p className='nav__welcome'>Hello <span className='nav__username'>{context.user.username}</span></p>
-      <Button icon>
+      <Button icon onClick={handleLogOut}>
         <Icon icon={faSignOutAlt} size="lg"/>
       </Button>
     </nav>
   );
 }
 
-export default Nav;
+export default Nav
