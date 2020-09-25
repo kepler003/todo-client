ebf9ceb57bf3047f97d4dd3ac61445e991e5e62b
Bartłomiej Pyzocha <b.pyzocha@gmail.com>
2020-09-25 13:48:35 +0200

    Create basic routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
 import LogIn from './components/pages/LogIn';
 import SignUp from './components/pages/SignUp';
 import UserProfile from './components/pages/UserProfile';
@@ -6,7 +13,19 @@ import UserProfile from './components/pages/UserProfile';
 function App() {
   return (
     <div className='container'>
-      <LogIn/>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <LogIn/>
+          </Route>
+          <Route path='/signup'>
+            <SignUp/>
+          </Route>
+          <Route path='/profile'>
+            <Redirect to='/'/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
