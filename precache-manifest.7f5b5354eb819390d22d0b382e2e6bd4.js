300ea47aef1672b192c86d1fdbe211d76ddbad59
Bartłomiej Pyzocha <b.pyzocha@gmail.com>
2020-09-26 00:54:58 +0200

    Updates

---
+++ precache-manifest.7f5b5354eb819390d22d0b382e2e6bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713de002513e6f11ee7c8759aad2e68e",
+    "url": "/todo-client/index.html"
+  },
+  {
+    "revision": "e91e8af6cfe0d5e4de0f",
+    "url": "/todo-client/static/css/main.34228ee0.chunk.css"
+  },
+  {
+    "revision": "e69e3982e39961de721a",
+    "url": "/todo-client/static/js/2.1c68dd2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-client/static/js/2.1c68dd2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e91e8af6cfe0d5e4de0f",
+    "url": "/todo-client/static/js/main.7383a948.chunk.js"
+  },
+  {
+    "revision": "6d1af829b894c8b808b0",
+    "url": "/todo-client/static/js/runtime-main.b260040c.js"
+  }
+]);
