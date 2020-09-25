d856b7b95d72e96617bf9e2dc13429d5a8065016
Bartłomiej Pyzocha <b.pyzocha@gmail.com>
2020-09-25 19:50:19 +0200

    Cosmetic change

--- src/components/organisms/SignUpDialog.js
+++ src/components/organisms/SignUpDialog.js
@@ -1,13 +1,62 @@
 
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import {useHistory} from 'react-router-dom';
+import axios from 'axios';
 
+import AppContext from '../../contexts/AppContext';
 import Button from '../atoms/Button';
 import InputBox from '../molecules/InputBox';
 
 function SignUpDialog() {
 
+  const {setContext} = useContext(AppContext);
   const history = useHistory();
+  const [data, setData] = useState({username: null, password: null})
+
+  const handleSubmit = async (e) => {
+    
+    try {
+      
+      e.preventDefault();
+
+      const response = (await axios.post('/user', {
+        username: data.username,
+        password: data.password
+      })).data;
+
+      setContext({
+        user: {
+          id: response.id,
+          username: response.username
+        },
+        notes: null
+      })
+
+    } catch (err) {
+
+      console.log(err);
+    }
+  }
+
+  const handleUsernameChange = (e) => {
+    
+    e.persist();
+    
+    setData(prev => ({
+      ...prev,
+      username: e.target.value
+    }))
+  }
+
+  const handlePasswordChange = (e) => {
+    
+    e.persist();
+
+    setData(prev => ({
+      ...prev,
+      password: e.target.value
+    }))
+  }
 
   const handleGoToLogIn = (e) => {
     e.preventDefault();
@@ -19,11 +68,11 @@ function SignUpDialog() {
       <form className='dialog'>
         <h1 className='dialog__heading'>Załóż konto</h1>
 
-        <InputBox label='Podaj nazwę użytkownika'/>
-        <InputBox label='Podaj hasło' password/>
+        <InputBox label='Podaj nazwę użytkownika' onChange={handleUsernameChange}/>
+        <InputBox label='Podaj hasło' password onChange={handlePasswordChange}/>
         
         <div className='dialog__action-btns'>
-          <Button secondary min>Załóż konto</Button>
+          <Button secondary min onClick={handleSubmit}>Załóż konto</Button>
           <span className='dialog__separator'>lub</span>
           <Button colorPrimary min onClick={handleGoToLogIn}>Zaloguj się</Button>
         </div>
